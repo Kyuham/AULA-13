d84d976d5e03b6671183ef43fc616e425f69700e
Kyuham <hiagovieiradesouza01@gmail.com>
2024-07-26 16:57:24 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,65 +1,37 @@
 <?php
 
-$teste="a,b,c";
+$nota1="A";
 
-switch ($teste) {
-    case "a,b,c":
-        echo "Entrou no primeiro case";
+
+switch ($nota1) {
+case "A":
+    echo "aprovado";
+    break;
+
+case "B":
+    echo "aprovado";
+    break;
+
+case "c":
+    echo "aprovado";
+    break;
+
+case "D":
+    echo "reprovado";
+    break;
+
+    case "E":
+    echo "reprovado";
+    break;
+
+    case "F":
+    echo "reprovado";
+    break;
+
+    default:
+        echo "entrou no default";
         break;
 
-        case 1;
-            echo "Entrou no case 1";
-            break;
-
-            default:
-            echo "Entrou no default";
-            break;
-}
-?>
-
-
-<?php
-
-$dia=12;
-
-switch ($dia) {
-    case 1:
-        echo "Domingo";
-        break;
-
-
-        case 2:
-            echo "Segunda";
-            break;
-
-
-            case 3:
-                echo "Terça";
-                break;
-
-
-                case 4:
-                    echo "Quarta";
-                    break;
-
-
-                    case 5:
-                        echo "Quinta";
-                        break;
-
-
-                        case 6:
-                            echo "Sexta";
-                            break;
-
-
-                            case 7:
-                                echo "Sábado";
-                                break;
-
-                                default:
-                                echo "Número Invalido. Digite um número de 1 a 7";
-                                break;
-}
-
-?>
+    
+                }
+                  ?>
